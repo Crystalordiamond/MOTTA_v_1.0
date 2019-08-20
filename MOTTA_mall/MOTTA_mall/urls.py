ea7f3d326f11017d8b3ecf3564f77d9fc974bf3e
办公室 <luochenxi163@163.com>
2019-08-20 10:05:05 +0800

    motta_v_1.0 回退了版本

--- MOTTA_mall/MOTTA_mall/urls.py
+++ MOTTA_mall/MOTTA_mall/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,5 +25,6 @@ urlpatterns = [
     url('^', include('wsdata.urls')),
     url('^', include('warning.urls')),
     url('^', include('divices.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 
 ]
